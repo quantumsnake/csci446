453f26f1788703797313cd5cdb18681da7a8fe22
Brennan Kimura <bkimura@mymail.mines.edu>
2012-01-21 10:34:57 -0700

    Finished the extra credit part for ex1.rb

--- project02/ex1.rb
+++ project02/ex1.rb
@@ -5,3 +5,9 @@ puts "This is fun."
 puts "Yay! Printing."
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+#Extra Credit Begins
+puts "This is the begining of EXTRA CREDIT!"
+#Second Part of the Extra Credit
+puts "Yay! Printing."
+#The octothorpe makes comments
+puts "'#' comments out everything right of it on a line"
