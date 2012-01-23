83c98467f0c86b5485b46a1e183e0025d07c09e0
Brennan Kimura <bkimura@mymail.mines.edu>
2012-01-23 16:09:03 -0700

    Beat level 6, the hardest part about this level was figuring out the correct health points for retreating and resting.

--- project02/rubywarrior/mr-t-beginner/player.rb
+++ project02/rubywarrior/mr-t-beginner/player.rb
@@ -2,26 +2,29 @@ class Player
   @health = nil
   def play_turn(warrior)
     @health = warrior.health unless @health
-    if warrior.feel.captive?
-      warrior.rescue!
-    else
+    
     if warrior.feel.enemy?
-      warrior.attack!
-    else
-      if warrior.health <= 14
-        if warrior.health < @health
-          if warrior.feel.enemy?
-            warrior.attack!
-          end
-          warrior.walk!
-        else
-          warrior.rest!
-        end
-      else
-      warrior.walk!
-      end
-    end
-    end
+   	  warrior.attack!
+   	else
+   	  if warrior.feel.captive?
+   	    warrior.resue!
+   	  else
+   	    if @health > warrior.health
+   	      if @health <= 8
+   	        warrior.walk! :backward
+   	      else
+   	        warrior.walk! 
+   	      end
+   	    else
+   	      if @health < 16
+   	    	warrior.rest!
+   	      else
+   	        warrior.walk!
+   	      end
+   	    end
+   	  end
+   	end
+   	      
     @health = warrior.health
   end
 end
