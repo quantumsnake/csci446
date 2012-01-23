adef40b86cdf03402ce844539d2f7ec3f66f4ed1
Brennan Kimura <bkimura@mymail.mines.edu>
2012-01-22 21:44:33 -0700

    Just finished level 4, figuring out how to use instance variables was the trick.

--- project02/rubywarrior/mr-t-beginner/player.rb
+++ project02/rubywarrior/mr-t-beginner/player.rb
@@ -1,13 +1,23 @@
 class Player
+  @health = nil
   def play_turn(warrior)
+    @health = warrior.health unless @health
     if warrior.feel.enemy?
       warrior.attack!
     else
       if warrior.health <= 14
-        warrior.rest!
+        if warrior.health < @health
+          if warrior.feel.enemy?
+            warrior.attack!
+          end
+          warrior.walk!
+        else
+          warrior.rest!
+        end
       else
       warrior.walk!
       end
     end
+    @health = warrior.health
   end
 end
