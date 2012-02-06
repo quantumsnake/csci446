e20854e4faf8777639b65c42c2beb0139245aea4
Brennan Kimura <bkimura@mymail.mines.edu>
2012-02-05 23:14:40 -0700

    Just generated the scaffold for Product

---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project03::Application.config.secret_token = '8a899050ba51ff2d0324cdeccd58acf8cf3de0851c3518c660cf449fc28b7ca744382bfb00eee655881187acd0ba1c905ffbbbf1d21d5cf13e4cee4574bced94'
