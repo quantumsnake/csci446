23c929990646342125926854d8ba0f91626d314c
Brennan Kimura <bkimura@mymail.mines.edu>
2012-02-07 16:42:42 -0700

    Just finished addin ajax in chapter 11, still have those failures

--- project03/app/controllers/carts_controller.rb
+++ project03/app/controllers/carts_controller.rb
@@ -81,7 +81,7 @@ class CartsController < ApplicationController
     @cart.destroy
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
+      format.html { redirect_to store_url }
       
       format.json { head :no_content }
     end
