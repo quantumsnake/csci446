4669df6bedd511451de608b202099f15a317faea
Brennan Kimura <bkimura@mymail.mines.edu>
2012-02-07 23:03:31 -0700

    restarting the webserver

--- project03/app/controllers/orders_controller.rb
+++ project03/app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.paginate page: params[:page], order: 'created_at desc', per_page: 10
+    @orders = Order.paginate page: params[:page], order: 'created_at desc', per_page: 1
 
     respond_to do |format|
       format.html # index.html.erb
