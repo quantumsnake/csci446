a7cb4a71a1247f0811012d14014a4a2b9cd28cd6
Brennan Kimura <bkimura@mymail.mines.edu>
2012-02-08 16:53:03 -0700

    Just created the admin side of the application: Admin can view orders, users and products.  Along with viewing them, admin can alter them to however they please.

--- project03/config/routes.rb
+++ project03/config/routes.rb
@@ -1,4 +1,14 @@
 Project03::Application.routes.draw do
+  get "admin/index"
+
+  get 'admin' => 'admin#index'
+  
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  
   resources :users
 
   resources :orders
