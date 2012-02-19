63a6ec38a8d41f7d331b4e91e6a69a37f8710be1
Brennan Kimura <bkimura@mymail.mines.edu>
2012-02-19 16:07:32 -0700

    Just created the articles app:  Time to make the magic happen.

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '4bfd64bf457272221926118a52ac09f37383f8ded643fc27e6306ecd588f8ce0dd88a7b972c40561586a860d18e4c664b0b3b057616462d2dc0bffec989b4fc3'
