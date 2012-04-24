6e335a21e961142c7f10d74ba8e4324dbdcfaa85
Brennan Kimura <bkimura@mymail.mines.edu>
2012-04-23 22:29:17 -0600

    Started Project07 and created a startNewGame functions

---
+++ project07/main.js
@@ -0,0 +1,31 @@
+var guessesLeft = 10;
+var theNumber = 0;
+var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
+
+$(function() {
+  updateScore(guessesLeft);
+  populateHighScores(highScores);
+
+});
+
+function startNewGame() {
+	// Resets the number of guesses
+	guessesLeft = 10;
+	
+	// Updates the game
+	updateScore(guessesLeft);
+	populateHighScores(highScores);
+	
+	// This generates the number to guess
+	theNumber = Math.floor((Math.random()*100)+1);
+}
+
+function populateHighScores(scores) {
+  for (var i = 0; i < scores.length; ++i) {
+    $('div#highScores').append("<p>" + scores[i][0] + " " + scores[i][1] + "</p>");
+  }
+}
+
+function updateScore(score) {
+  $('h2#score span#guessesLeft').append(score);
+}
